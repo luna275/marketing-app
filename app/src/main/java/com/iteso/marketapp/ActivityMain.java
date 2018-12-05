e00c80b882b2439baa1e177e7af05158959ba273
ppgmz <ie697875@iteso.mx>
2018-12-04 18:23:51 -0600

    Added basic layout to app

--- app/src/main/java/com/iteso/marketapp/ActivityMain.java
+++ app/src/main/java/com/iteso/marketapp/ActivityMain.java
@@ -2,12 +2,29 @@ package com.iteso.marketapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class ActivityMain extends AppCompatActivity {
+
+    private Toolbar mTopToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
